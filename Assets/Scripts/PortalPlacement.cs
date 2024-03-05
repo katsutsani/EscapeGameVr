6c32740009494f497dad2dda1c9357f8b6eb9a9b
Guillaume <71400691+katsutsani@users.noreply.github.com>
2024-03-05 17:32:44 +0100

    portalgun for pc

---
+++ Assets/Scripts/PortalPlacement.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalPlacement : MonoBehaviour
+{
+
+    [SerializeField]
+    private PortalPair portals;
+
+    [SerializeField]
+    private LayerMask layerMask;
+
+    [SerializeField]
+    private Crosshair crosshair;
+
+    private CameraMove cameraMove;
+
+    private void Awake()
+    {
+        cameraMove = GetComponent<CameraMove>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            FirePortal(0, transform.position, transform.forward, 250.0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            FirePortal(1, transform.position, transform.forward, 250.0f);
+        }
+    }
+
+    private void FirePortal(int portalID, Vector3 pos, Vector3 dir, float distance)
+    {
+        RaycastHit hit;
+
+        Physics.Raycast(pos, dir, out hit, distance, layerMask);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.tag == "Portal")
+            {
+                var inPortal = hit.collider.GetComponent<Portal>();
+
+                if (inPortal == null)
+                {
+                    return;
+                }
+
+                var outPortal = inPortal.OtherPortal;
+
+                Vector3 relativePos = inPortal.transform.InverseTransformPoint(hit.point + dir);
+                relativePos = Quaternion.Euler(0.0f, 180.0f, 0.0f) * relativePos;
+                pos = outPortal.transform.TransformPoint(relativePos);
+
+                Vector3 relativeDir = inPortal.transform.InverseTransformDirection(dir);
+                relativeDir = Quaternion.Euler(0.0f, 180.0f, 0.0f) * relativeDir;
+                dir = outPortal.transform.TransformDirection(relativeDir);
+
+                distance -= Vector3.Distance(pos, hit.point);
+
+                FirePortal(portalID, pos, dir, distance);
+
+                return;
+
+            }
+
+            var cameraRotation = cameraMove.TargetRotation;
+            var portalRight = cameraRotation * Vector3.right;
+
+            if(Mathf.Abs(portalRight.x) >= Mathf.Abs(portalRight.z))
+            {
+                portalRight = (portalRight.x >= 0) ? Vector3.right : -Vector3.right;
+
+            }
+            else
+            {
+                portalRight = (portalRight.z >= 0) ? Vector3.forward : -Vector3.forward;
+
+            }
+
+            var portalForward = -hit.normal;
+
+            var portalUp = -Vector3.Cross(portalRight, portalForward);
+
+            var portalRotation = Quaternion.LookRotation(portalForward, portalUp);
+
+            bool wasPlaced = portals.Portals[portalID].PlacePortal(hit.collider, hit.point, portalRotation);
+
+            if(wasPlaced )
+            {
+                crosshair.SetPortalPlaced(portalID, true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PortalableObject.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalableObject : MonoBehaviour
+{
+
+    private GameObject cloneObject;
+
+    private int inPortalCount = 0;
+
+    private Portal inPortal;
+
+    private Portal outPortal;
+
+    private new Rigidbody body;
+    protected new Collider collider;
+
+    private static readonly Quaternion halfTurn = Quaternion.Euler(0f, 180.0f, 0f);
+
+    private void Awake()
+    {
+        cloneObject = new GameObject();
+        cloneObject.SetActive(false);
+        var meshFilter = cloneObject.AddComponent<MeshFilter>();
+        var meshRenderer = cloneObject.AddComponent<MeshRenderer>();
+
+        meshFilter.mesh = GetComponent<MeshFilter>().mesh;
+        meshRenderer.materials = GetComponent<MeshRenderer>().materials;
+
+        cloneObject.transform.localScale = transform.localScale;
+
+        body = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+    }
+
+    private void LateUpdate()
+    {
+        if(inPortal == null || outPortal == null) return;
+
+        if(cloneObject.activeSelf && inPortal.IsPlaced && outPortal.IsPlaced)
+        {
+            var inTransform = inPortal.transform;
+            var outTransform = outPortal.transform;
+
+            Vector3 relativePos = inTransform.InverseTransformPoint(transform.position);
+            relativePos = halfTurn * relativePos;
+            cloneObject.transform.position = outTransform.TransformPoint(relativePos);
+
+            Quaternion relativeRot = Quaternion.Inverse(inTransform.rotation) * transform.rotation;
+            relativeRot = halfTurn * relativeRot;
+            cloneObject.transform.rotation = outTransform.rotation * relativeRot;
+        }
+        else
+        {
+            cloneObject.transform.position = new Vector3(-1000.0f, 1000.0f, -1000.0f);
+        }
+    }
+
+    public void SetIsInPortal(Portal inPortal, Portal outPortal, Collider wallCollider)
+    {
+        this.inPortal= inPortal;
+        this.outPortal= outPortal;
+
+        Physics.IgnoreCollision(collider, wallCollider);
+
+        cloneObject.SetActive(false);
+
+        ++inPortalCount;
+    }
+
+    public void ExitPortal(Collider wallCollider)
+    {
+        Physics.IgnoreCollision(collider, wallCollider, false);
+
+        --inPortalCount;
+
+        if(inPortalCount == 0)
+        {
+            cloneObject.SetActive(false);
+        }
+    }
+
+    public virtual void Warp()
+    {
+        var inTransform = inPortal.transform;
+        var outTransform = outPortal.transform;
+
+        Vector3 relativePos = inTransform.InverseTransformPoint(transform.position);
+        relativePos = halfTurn * relativePos;
+        transform.position = outTransform.TransformPoint(relativePos);
+
+        Quaternion relativeRot = Quaternion.Inverse(inTransform.rotation) * transform.rotation;
+        relativeRot = halfTurn * relativeRot;
+        transform.rotation = outTransform.rotation * relativeRot;
+
+        Vector3 relativeVel = inTransform.InverseTransformDirection(body.velocity);
+        relativeVel = halfTurn * relativeVel;
+        body.velocity = outTransform.TransformDirection(relativeVel);
+
+        var tmp = inPortal;
+        inPortal = outPortal; 
+        outPortal = tmp;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
