5ce17cd1809c97094fa646179dbb8fa1a4370381
atn3126 <92393048+atn3126@users.noreply.github.com>
2024-03-06 12:24:17 +0100

    FirstAImove

--- Assets/Scenes/AIScene/AI_Follow.cs
+++ Assets/Scenes/AIScene/AI_Follow.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    public Transform ObjectTransform;
+    NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = ObjectTransform.position;
     }
 }
