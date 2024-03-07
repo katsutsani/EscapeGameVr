f25a0c97ae809f07b1dd939bed1570bb00e46a8d
OshidaBCF <swann.sanchez9@outlook.fr>
2024-03-07 10:21:51 +0100

    Init Push
    I forgot to push the branch yesterday
    
    Button isn't working because controllers cannot interact with anything

---
+++ Assets/Swann/script/buttonVR.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class buttonVR : MonoBehaviour
+{
+    public GameObject button;
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+    GameObject presser;
+    bool isPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPressed = false;
+    }
+    /*
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isPressed)
+        {
+            presser = other.gameObject;
+            onPress.Invoke();
+            isPressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == presser)
+        {
+            onRelease.Invoke();
+            isPressed = false;
+        }
+    }*/
+
+    public void spawnSphere()
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        sphere.transform.localPosition = new Vector3(0, 1, 2);
+        sphere.AddComponent<Rigidbody>();
+    }
+}
