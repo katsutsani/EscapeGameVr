dfd8b30c48b1aee02e9b3697ba707a0e27bd91f0
atn3126 <92393048+atn3126@users.noreply.github.com>
2024-03-08 14:12:28 +0100

    KyleRobot Assets

--- Assets/Scenes/AIScene/AI_Follow.cs
+++ Assets/Scenes/AIScene/AI_Follow.cs
@@ -8,16 +8,19 @@ public class NewBehaviourScript : MonoBehaviour
 
     public Transform ObjectTransform;
     NavMeshAgent agent;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.destination = ObjectTransform.position;
+        animator.SetFloat("Speed", agent.velocity.magnitude);
     }
 }
